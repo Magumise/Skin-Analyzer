1837302e703d3ba68961c79f45dddd55e7b8c469
Your Name <your.email@example.com>
2025-05-28 09:58:59 +0300

    Remove signals import from apps.py

--- backend/analyzer/apps.py
+++ backend/analyzer/apps.py
@@ -5,6 +5,3 @@ class AnalyzerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analyzer'
     verbose_name = 'AI Skin Analyzer'
-
-    def ready(self):
-        import analyzer.signals  # noqa
